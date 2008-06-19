9938db6f92620f71ead6f1698df4b05dc47b9f5f
Chris Porter <slug@qwebirc.org>
2008-06-19 20:26:17 +0100

    SWMUI complete?

--- js/ui/swmlayout.js
+++ js/ui/swmlayout.js
@@ -5,25 +5,36 @@ var SWM_ANCHOR_LEFT =   0x04;
 var SWM_ANCHOR_RIGHT =  0x08;
 
 var SWMPanel = new Class({
-  initialize: function(parent, id) {
+  initialize: function(parent, hidden) {
     this.parent = parent;
     
-    var element = new Element("div", {"id": id, "styles": { "position": "absolute" } });
+    var element = new Element("div", {"styles": { "position": "absolute" } });
     this.element = element;
     this.element.wmpanel = this;
-
-    parent.addClass("wmelement");
+    
+    if(hidden) {
+      this.setHidden(true);
+    } else {
+      this.hidden = false;
+    }
+    parent.addClass("swmelement");
     
     parent.appendChild(this.element);
-    this.anchor = ANCHOR_NONE;
+    this.anchor = SWM_ANCHOR_NONE;
   },
-
   setHeight: function(height) {
     this.height = height;
   },
-    
   setWidth: function(width) {
     this.width = width;
+  },
+  setHidden: function(value) {
+    this.hidden = value;
+    if(value) {
+      this.element.setStyle("display", "none");
+    } else {
+      this.element.setStyle("display", "block");
+    }
   }
 });
 
@@ -47,39 +58,49 @@ window.addEvent("domready", function() {
     
     var tpos = 0;
     for(var i=0;i<top.length;i++) {
+      if(top[i].hidden)
+        continue;
       var obj = top[i].element;
       obj.setStyles({"top": tpos + "px", "left": "0px", "right": "0px"});
-      tpos = tpos + obj.getSize()["size"]["y"];
+      tpos = tpos + obj.getSize().y;
     }
     
     var bpos = 0;
     for(var i=0;i<bottom.length;i++) {
+      if(bottom[i].hidden)
+        continue;
       var obj = bottom[i].element;
       obj.setStyles({"bottom": bpos + "px", "left": "0px", "right": "0px"});
-      bpos = bpos + obj.getSize()["size"]["y"];
+      bpos = bpos + obj.getSize().y;
     }
     
     var lpos = 0;
     for(var i=0;i<left.length;i++) {
+      if(left[i].hidden)
+        continue;
       var obj = left[i].element;
       obj.setStyles({"left": lpos + "px", "top": tpos + "px", "bottom": bpos + "px"});
-      lpos = lpos + obj.getSize()["size"]["x"];
+      lpos = lpos + obj.getSize().x;
     }
 
     var rpos = 0;
     for(var i=0;i<right.length;i++) {
+      if(right[i].hidden)
+        continue;
       var obj = right[i].element;
       obj.setStyles({"right": rpos + "px", "top": tpos + "px", "bottom": bpos + "px"});
-      rpos = rpos + obj.getSize()["size"]["x"];
+      rpos = rpos + obj.getSize().x;
     }
 
     for(var i=0;i<none.length;i++) {
+      if(none[i].hidden)
+        continue;
       var obj = none[i].element;
       obj.setStyles({"left": lpos + "px", "right": rpos + "px", "top": tpos + "px", "bottom": bpos + "px"});
     }
   }
   
   window.addEvent("resize", function() {
-    $$("div[class=wmelement]").forEach(reworklayout);
+    $$("div.swmelement").each(reworkLayout);
   });
 });
--- js/ui/swmui.js
+++ js/ui/swmui.js
@@ -1,54 +1,176 @@
-var RootPanel;
-var TabPanel;
-var EntryPanel;
-var MainPanel;
-var NickList;
-var Content;
+var SWMUIWindow = new Class({
+  Extends: UIWindow,
+  
+  initialize: function(parentObject, client, type, name) {
+    this.parent(parentObject, client, type, name);
+    this.contentPanel = new SWMPanel(parentObject.mainPanel.element, true);
+    this.contentPanel.element.addClass("content");
+    this.contentPanel.element.setStyle("overflow", "auto");
 
-window.addEvent("domready", function() {
-  RootPanel = $("container");
-  TabPanel = new Panel(RootPanel, "tabs");
-  
-  TabPanel.anchor = ANCHOR_TOP;
-  MainPanel = new Panel(RootPanel, "main");
-  
-  EntryPanel = new Panel(RootPanel, "entry");
-  EntryPanel.anchor = ANCHOR_BOTTOM;
-  
-  //EntryPanel.element.setHTML("<input/>");
-  EntryPanel.element.setHTML("<input class=\"inputwidth\"/>");
-  
-  var iw = $$("input[class=inputwidth]");
-  window.addEvent("resize", function() {
-    var s = EntryPanel.element.getSize()["size"]["x"] - 4;
-    iw.setStyle("width", s + "px");
-  });
-  //window.addEvent("domready", function() {
-  
-  Content = new Panel(MainPanel.element, "content");
-  Content.element.setStyle("overflow", "auto");
-  
-  NickList = new Panel(MainPanel.element, "nicklist");
-  NickList.anchor = ANCHOR_RIGHT;
-  NickList.element.setStyle("overflow", "auto");
-  
-  for(var i=0;i<100;i++) {
-    var e = new Element("div").setText("MOO");
-    
-    Content.element.appendChild(e);
-  }
-  
-  for(var i=0;i<10;i++) {
-    var e = new Element("div").setText("MOO");
-    
-    NickList.element.appendChild(e);
-  }
+    if(type == WINDOW_CHANNEL) {
+      this.nickList = new SWMPanel(this.contentPanel.element);
+      this.nickList.anchor = SWM_ANCHOR_RIGHT;
+      this.nickList.element.setStyle("overflow", "auto");
+      this.nickList.element.addClass("nicklist");
+    }
 
-  for(var i=0;i<5;i++) {
-    var e = new Element("span").setText("MOO" + i);
+    if(type == WINDOW_CHANNEL) {
+      this.topic = new SWMPanel(this.contentPanel.element);
+      this.topic.anchor = SWM_ANCHOR_TOP;
+      this.topic.element.addClass("topic");
+    }
     
-    TabPanel.element.appendChild(e);
+    this.lines = new SWMPanel(this.contentPanel.element);
+    this.lines.element.setStyle("overflow", "auto");
+    
+    this.tab = new Element("span");
+    this.tab.addClass("tab");
+    
+    this.tab.appendText(name);
+    this.tab.addEvent("click", function() {
+      parentObject.selectWindow(this);
+    }.bind(this));
+
+    parentObject.tabPanel.element.appendChild(this.tab);
+    parentObject.resize();
+    
+    if(type != WINDOW_STATUS) {
+      tabclose = new Element("span");
+      tabclose.addClass("tabclose");
+      tabclose.addEvent("click", function(e) {
+        new Event(e).stop();
+        
+        if(type == WINDOW_CHANNEL)
+          this.client.exec("/PART " + name);
+
+        this.close();
+      }.bind(this));
+      tabclose.set("text", "X");
+      this.tab.appendChild(tabclose);
+    }
+  },
+  updateNickList: function(nicks) {
+    this.parent(nicks);
+
+    var n = this.nickList.element;
+    while(n.firstChild)
+      n.removeChild(n.firstChild);
+
+    nicks.each(function(nick) {
+      var e = new Element("div");
+      n.appendChild(e);
+      e.appendChild(document.createTextNode(nick));
+    });
+    
+    parentObject.resize();
+  },
+  updateTopic: function(topic) {
+    this.parent(topic);
+
+    var t = this.topic.element;
+    
+    while(t.firstChild)
+      t.removeChild(t.firstChild);
+
+    this.parentObject.resize();
+    Colourise(topic, t);
+  },
+  select: function() {
+    this.parent();
+
+    this.contentPanel.setHidden(false);
+    this.parentObject.resize();
+    this.tab.removeClass("tab-highlighted");
+    this.tab.removeClass("tab-unselected");
+    this.tab.addClass("tab-selected");
+  },
+  deselect: function() {
+    this.parent();
+
+    this.contentPanel.setHidden(true);
+    this.parentObject.resize();
+    this.tab.removeClass("tab-selected");
+    this.tab.addClass("tab-unselected");
+  },
+  close: function() {
+    this.parent();
+
+    this.parentObject.mainPanel.element.removeChild(this.contentPanel.element);
+    this.parentObject.tabPanel.element.removeChild(this.tab);
+  },
+  addLine: function(type, line, colour) {
+    this.parent(type, line, colour);
+    
+    var e = new Element("div");
+
+    if(colour) {
+      e.addStyle("background", colour);
+    } else if(this.lastcolour) {
+      e.addClass("linestyle1");
+    } else {
+      e.addClass("linestyle2");
+    }
+    
+    if(type)
+      line = this.parentObject.theme.message(type, line);
+    
+    Colourise(IRCTimestamp(new Date()) + " " + line, e);
+    
+    this.lastcolour = !this.lastcolour;
+    
+    var prev = this.lines.element.getScroll();
+    var prevbottom = this.lines.element.getScrollSize().y;
+    var prevsize = this.lines.element.getSize();
+    this.lines.element.appendChild(e);
+    
+    if(prev.y + prevsize.y == prevbottom)
+      this.lines.element.scrollTo(prev.x, this.lines.element.getScrollSize().y);
+      
+    if(!this.active)
+      this.tab.addClass("tab-highlighted");
   }
-  
-  window.fireEvent("resize");
+});
+
+var SWMUI = new Class({
+  Extends: UI,
+  initialize: function(parentElement, theme) {
+    this.parent(parentElement, SWMUIWindow, "swmui");
+    this.theme = theme;
+    
+    this.tabPanel = new SWMPanel(parentElement);
+    this.tabPanel.anchor = SWM_ANCHOR_TOP;
+    this.tabPanel.element.addClass("tabs");
+    
+    this.mainPanel = new SWMPanel(parentElement);
+    this.mainPanel.element.addClass("main");
+    
+    this.entryPanel = new SWMPanel(parentElement);
+    this.entryPanel.anchor = SWM_ANCHOR_BOTTOM;
+    this.entryPanel.element.addClass("entry");
+
+    var form = new Element("form");
+    
+    var inputbox = new Element("input");
+    
+    window.addEvent("resize", function() {
+      var s = this.entryPanel.element.getSize().x - 4;
+      inputbox.setStyle("width", s + "px");
+    }.bind(this));
+
+    form.addEvent("submit", function(e) {
+      new Event(e).stop();
+    
+      this.getActiveWindow().client.exec(inputbox.value);
+      inputbox.value = "";
+    }.bind(this));
+
+    this.entryPanel.element.appendChild(form);
+    form.appendChild(inputbox);
+    inputbox.focus();
+
+    this.resize();
+  },
+  resize: function() {
+    window.fireEvent("resize");
+  }
 });
