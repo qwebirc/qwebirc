b481a99a735825756a5ee72910b7d8f7abb2cf6d
Chris Porter <slug@qwebirc.org>
2008-06-19 18:27:44 +0100

    Reorganise.

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 @echo off
 del compiled.js
 del concat.js
-cd static\js
-copy version.js + jslib.js + irc\ircconnection.js + irc\irclib.js + irc\baseirc.js + irc\irctracker.js + irc\commandparser.js + irc\ircclient.js + ui\uibase.js + ui\colour.js + ui\theme.js + ui\uglyui.js ..\..\concat.js /b
-cd ..\..
+cd js
+copy version.js + jslib.js + irc\ircconnection.js + irc\irclib.js + irc\baseirc.js + irc\irctracker.js + irc\commandparser.js + irc\ircclient.js + ui\uibase.js + ui\colour.js + ui\theme.js + ui\uglyui.js ..\concat.js /b
+cd ..
 java -jar bin\yuicompressor-2.3.5.jar concat.js > compiled.js
 
 if %errorlevel% == 0 goto ok
@@ -14,6 +14,7 @@ goto ene
 
 :ok
 del concat.js
+copy js\copyright.js + compiled.js /b static\js\qwebirc.js
 goto end
 
 :end
---
+++ js/copyright.js
@@ -0,0 +1,2 @@
+/* qwebirc -- Copyright (C) 2008 Chris Porter --- All rights reserved. */
+
