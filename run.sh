676affb4778caa4d8a6c4c6b0708070da6642fdc
Chris Porter <slug@qwebirc.org>
2008-07-11 01:31:28 +0100

    Improve run.sh, also correct typo in example config.

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 export PYTHONPATH=.:${PYTHONPATH}
-twistd qwebirc -n
+
+if [ -f "defargs.conf" ]; then
+  ARGS=$(cat defargs.conf)
+else
+  ARGS=
+fi
+
+PREARGS=
+if [ "$1" = "-n" ]; then
+  PREARGS=$1
+  shift
+fi
+
+if [ "$1" != "" ]; then
+  if [ "$ARGS" != "" ]; then
+    echo "Not using default arguments: $ARGS"
+  fi
+  ARGS=$*
+fi
+
+twistd $PREARGS qwebirc -n $ARGS
