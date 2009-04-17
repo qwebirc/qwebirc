195fe8620d2ed35777c8db3ae485af6259a3a20e
Chris Porter <slug@qwebirc.org>
2009-04-17 14:14:25 +0100

    Fix WEBIRC line, thanks thommey.

--- qwebirc/ircclient.py
+++ qwebirc/ircclient.py
@@ -80,7 +80,7 @@ class QWebIRCClient(basic.LineReceiver):
       hmac = hmacfn(ident, ip)
       self.write("USER %s bleh bleh %s %s :%s" % (ident, ip, hmac, realname))
     elif config.WEBIRC_MODE == "webirc":
-      self.write("WEBIRC %s qwebirc %s %s" % (config.WEBIRC_PASSWORD, ip, hostname))
+      self.write("WEBIRC %s qwebirc %s %s" % (config.WEBIRC_PASSWORD, hostname, ip))
       self.write("USER %s bleh %s :%s" % (ident, ip, realname))
     else:
       if ip == hostname:
