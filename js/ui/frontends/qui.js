0a55be30fb8765bb916f218e111c6ec063a8bf16
Chris Porter <slug@qwebirc.org>
2009-06-21 17:28:40 +0100

    Add a reflow after a delay for Konqueror.

--- js/ui/frontends/qui.js
+++ js/ui/frontends/qui.js
@@ -49,7 +49,8 @@ qwebirc.ui.QUI = new Class({
     
     this.createInput();
     this.reflow();
-
+    this.reflow.delay(100); /* Konqueror fix */
+    
     /* HACK, in Chrome this should work immediately but doesn't */
     this.__createDropdownHint.delay(100, this);
   },
