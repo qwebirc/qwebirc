2c9bf6032fa602d1d74ffd36ade93b06e58fde91
Malcolm Scott <mas90@users.noreply.github.com>
2018-01-08 00:38:41 +0000

    dns: don't assume that all answers are of the requested type (i.e. support CNAME/DNAME)

--- qwebirc/dns.py
+++ qwebirc/dns.py
@@ -1,4 +1,4 @@
-from twisted.names import client
+from twisted.names import client, dns
 from twisted.internet import reactor, defer
 
 class LookupException(Exception): pass
@@ -8,7 +8,7 @@ TimeoutException = defer.TimeoutError
 def lookupPTR(ip, *args, **kwargs):
   def callback(result):
     answer, auth, add = result
-
+    answer = [x for x in answer if x.type == dns.PTR]
     if len(answer) == 0:
       raise LookupException, "No ANSWERS in PTR response for %s." % repr(ip)
     return str(answer[0].payload.name)
@@ -25,7 +25,7 @@ def expandIPv6(ip):
 def lookupPTRv6(ip, *args, **kwargs):
   def callback(result):
     answer, auth, add = result
-
+    answer = [x for x in answer if x.type == dns.PTR]
     if len(answer) == 0:
       raise LookupException, "No ANSWERS in PTR response for %s." % repr(ip)
     return str(answer[0].payload.name)
@@ -36,6 +36,7 @@ def lookupPTRv6(ip, *args, **kwargs):
 def lookupAs(hostname, *args, **kwargs):
   def callback(result):
     answer, auth, add = result
+    answer = [x for x in answer if x.type == dns.A]
     if len(answer) == 0:
       raise LookupException, "No ANSWERS in A response for %s." % repr(hostname)
     return [x.payload.dottedQuad() for x in answer]
@@ -45,6 +46,7 @@ def lookupAs(hostname, *args, **kwargs):
 def lookupAAAAs(hostname, *args, **kwargs):
   def callback(result):
     answer, auth, add = result
+    answer = [x for x in answer if x.type == dns.AAAA]
     if len(answer) == 0:
       raise LookupException, "No ANSWERS in AAAA response for %s." % repr(hostname)
     return [expandIPv6(x.payload._address) for x in answer]
