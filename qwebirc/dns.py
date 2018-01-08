9b03b0d7f41e6e60f79659dd7de39aa99ba57cf7
Malcolm Scott <mas90@users.noreply.github.com>
2018-01-08 00:45:49 +0000

    dns: cope with IPv6 addresses that don't contain ::

--- qwebirc/dns.py
+++ qwebirc/dns.py
@@ -17,10 +17,12 @@ def lookupPTR(ip, *args, **kwargs):
   return client.lookupPointer(ptr, **kwargs).addCallback(callback)
 
 def expandIPv6(ip):
-  expand_start, expand_end = ["".join(["{:0>4}".format(group)
+  expand_sections = ["".join(["{:0>4}".format(group)
       for group in section.split(":")])
         for section in ip.split("::")]
-  return expand_start + (32-len(expand_start)-len(expand_end))*"0" + expand_end
+  if len(expand_sections) == 1:
+      return expand_sections[0]
+  return expand_sections[0] + (32-len(expand_start)-len(expand_end))*"0" + expand_sections[1:]
 
 def lookupPTRv6(ip, *args, **kwargs):
   def callback(result):
